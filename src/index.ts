6ac0ad4bcc24e0f06af5fdb5d3fffc7b78c41e0f
junzzi <jjh01027717502@gmail.com>
2022-04-05 16:28:18 +0900

    배포

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,3 @@ import '../images/empty-img.png';
 import '../src/styles/index.css';
 import AppComponent from './components';
 const appComponent = new AppComponent();
-
-const vendingMachineComponent = new VendingMachineComponent();
-window.addEventListener('popstate', e => {
-  const { state } = e;
-  vendingMachineComponent.showSection(state?.path ?? TAB_NAME.MANAGE);
-});
